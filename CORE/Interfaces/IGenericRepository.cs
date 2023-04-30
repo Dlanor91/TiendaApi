ed67629e4731c97bf7cf32b77963560f4df7dd54
Ronald <rl8506@gmail.com>
2023-04-30 15:34:14 -0300

    interfaz y repositorio generico

---
+++ CORE/Interfaces/IGenericRepository.cs
@@ -0,0 +1,22 @@
+﻿using CORE.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CORE.Interfaces
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IEnumerable<T>> GetAllAsync();
+        IEnumerable<T> Find(Expression<Func<T, bool>> expression);//depende de la consulta linq
+        void Add(T entity);
+        void AddRange(IEnumerable<T> entities);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+        void Update(T entity);
+    }
+}
