92842dcdae24f3934e83940d10df718e80a7bf35
Ronald <rl8506@gmail.com>
2023-04-30 14:51:34 -0300

    controller producto con get and get by id

---
+++ API/Controllers/ProductosController.cs
@@ -0,0 +1,37 @@
+﻿using CORE.Entities;
+using Infraestructura.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class ProductosController : BaseApiController
+    {
+        private readonly TiendaContext _context; // lo annado al controller
+        public ProductosController(TiendaContext context) //luego lo introduzco en el contructor
+        {
+            _context = context;
+        }
+
+        //metodo get
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<ActionResult<IEnumerable<Producto>>> Get()
+        {
+            var productos = await _context.Productos
+                            .ToListAsync();
+            return Ok(productos);
+        }
+
+        //get By id
+        [HttpGet("{id}")]//indico el parametro
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Get(int id) //es con IActionResult para un id
+        {
+            var producto = await _context.Productos.FindAsync(id);            
+            return Ok(producto);
+        }
+    }
+}
